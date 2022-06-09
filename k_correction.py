40ef41bd260773dc6240dfe14cf5c95d1f970c63
xiuhui@T470p <thewomanxh@gmail.com>
2022-06-09 11:59:14 +0800

    amend k-correction.

--- k_correction.py
+++ k_correction.py
@@ -13,6 +13,7 @@ fcsv = 'minijpas_pdr201912_cat_wAbsMag_v1.csv'
 fout = './Data_Output/'
 
 h = 0.6774
+num_bins = 8
 def calc_kcor(izz, rapp, iabs):
     Dl = Cfunc.Distfunc(izz, unit = 'Mpc', ih = h)
     k_04 = rapp - 5*np.log10(Dl) - 25. - iabs + 5*np.log10(h)
@@ -48,11 +49,14 @@ zz = photoz.drop(z0_index)
 
 ## bin g_r range
 #g_r_range = [-100, -0.18, 0.35, 0.52, 0.69, 0.86, 1.03, 100.]
-g_r_range = np.linspace(min(g_r), max(g_r), num = 8)
-g_r_bin_index = [[] for i in range(7)]
+g_r_range = np.linspace(min(g_r), max(g_r), num = num_bins + 1)
+g_r_bin_index = [[] for i in range(num_bins)]
+p1_txt = [[] for i in range(num_bins)]
+g_r_median = np.zeros(num_bins)
 
 plt.figure(1, (18, 16))
-for i in range(7):
+for i in range(num_bins):
+    g_r_median[i] = (g_r_range[i] + g_r_range[i + 1])/2.
     aa = (g_r[(g_r > g_r_range[i]) & (g_r <= g_r_range[i+1])]).index
     g_r_bin_index[i].append(aa)
     index = np.array(g_r_bin_index[i][0])
@@ -61,6 +65,7 @@ for i in range(7):
     #print(zz, k0)
 
     ## plot points.
+    """
     plt.plot(zz[index], k0, '.')
     plt.xlim([0, 1.5])
     plt.xticks([0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4], (r'$0$', r'$0.2$', r'$0.4$', r'$0.6$', r'$0.8$', r'$1.0$', r'$1.2$', r'$1.4$'), fontsize = 15)
@@ -68,21 +73,31 @@ for i in range(7):
     plt.ylabel(r'$k(z)$', fontsize = 17)
     plt.tight_layout()
     plt.show()
-   
+    """
+
     ## assign polynomial
     z1 = np.polyfit(zz[index], k0, 4)
     p1 = np.poly1d(z1)
+    p1_txt[i].append(z1)
     print(i, '\n', p1)
-    
+
+    ## plot polynomial
     #y = lambda x: z1[0]*x**4 + z1[1]*x**3 + z1[2]*x**2 + z1[3]*x + z1[4]
     xx = np.arange(0, 1.5, 0.02)
-    plt.plot(xx, p1(xx), '-')
-    plt.xlim([0, 1.5])
-    plt.xticks([0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4], (r'$0$', r'$0.2$', r'$0.4$', r'$0.6$', r'$0.8$', r'$1.0$', r'$1.2$', r'$1.4$'), fontsize = 15)
-    plt.xlabel(r'$z$', fontsize = 17)
-    plt.ylabel(r'$k(z)$', fontsize = 17)
-    plt.tight_layout()
-    plt.show()
+    plt.plot(xx, p1(xx), '-', label = r'$(g-r)_{med}=$%.3f'%g_r_median[i])
+
+np.savetxt('./Data_Output/Kcorrection/k_correction_polyfac.txt', np.hstack([g_r_range[:-1].reshape(8, 1), g_r_range[1:].reshape(8, 1), np.array(p1_txt).reshape(8, 5), g_r_median.reshape(8, 1)]), fmt = '%.9s')
+
+plt.xlim([0.15, 0.6])
+plt.xticks([0.2, 0.3, 0.4, 0.5, 0.6], (r'$0.2$', r'$0.3$', r'$0.4$', r'$0.5$', r'$0.6$'), fontsize = 15)
+plt.xlabel(r'$z$', fontsize = 17)
+plt.ylim([0, 5])
+plt.yticks([0, 1, 2, 3, 4, 5], (r'$0$', r'$1$', r'$2$', r'$3$', r'$4$', r'$5$'), fontsize = 15)
+plt.ylabel(r'$k(z)$', fontsize = 17)
+plt.legend(loc = 'best', frameon = False, fontsize = 8)
+plt.tight_layout()
+plt.savefig('./Data_Output/Kcorrection/k_correction_polyfit.png')
+plt.show()
 
 
 
